15fe47cfda25f60a984b7b5a6f34d2d6a186b77d
nicewangli <534662311@qq.com>
2020-05-27 18:43:19 +0800

    添加常量数组

--- app/model/Attendance.php
+++ app/model/Attendance.php
@@ -9,5 +9,26 @@ namespace app\model;
 class Attendance extends Model
 {
 
-    
+    public static function attendanceType()
+    {
+        return [
+            'O' => 'Day Off',
+            'AL' => '年假',
+            '*' => '補假',
+            'PL' => '事假',
+            'SL' => '病假',
+            'D' => '工作時間D',
+            'E' => '工作時間E',
+            'B' => '工作時間B',
+            'NP' => '無薪假',
+            'OT' => '超時工作',
+            'X' => '未到/已遇辦公時間',
+            'OF' => '不在公司',
+            'ER' => '補鐘',
+            'MT' => 'Meeting',
+            'TR' => 'Training',
+
+        ];
+    }
+
 }
